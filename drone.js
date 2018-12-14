b2dd5872f7f99792f19b6b9e45a006ce1026462c
Conrad Ludgate <oon@conradludgate.com>
2018-12-14 15:10:01 +0000

    Fix orbit

--- drone.js
+++ drone.js
@@ -3,7 +3,9 @@ class Drone {
 		this.orbit_r = 100;
 		this.orbit_o = createVector(width/2, height/2);
 
-		this.p = this.orbit_o; this.p.x += this.orbit_r;
+		this.p = createVector(this.orbit_r, 0);
+		this.p.add(this.orbit_o);
+		
 		this.v = createVector(0, -this.orbit_r);
 		this.a = createVector(-this.orbit_r, 0);
 
